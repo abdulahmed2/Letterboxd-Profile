9dfb717e6bbf62d0d131697a91584c1090e01b08
Abdul Ahmed <abdulrafay123ww@gmail.com>
2025-05-21 01:22:15 -0400

    trying to work on getting numbers for the ratings

--- scraper-python.py
+++ scraper-python.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape():
-    print('Enter your Letterboxd username. Case sensitive:')
+    print('Enter your Letterboxd username:')
     username = input()
     url = f'https://letterboxd.com/{username}/films/by/entry-rating/'
 
@@ -11,7 +11,6 @@ def scrape():
     
     num_of_pages = soup.find_all('li', class_="paginate-page")
     x = 1
-
     for i in num_of_pages:
         url = f'https://letterboxd.com/{username}/films/by/entry-rating/page/{x}/'
         response = requests.get(url)
@@ -21,8 +20,8 @@ def scrape():
             img = film.find('img')
             if img and img.has_attr('alt'):
                 print(img['alt'])
-            rating = film.find('p')
-            print(rating.text)
+            rating = film.find('span', class_='rating')
+            print(rating[-2] + '/10')
         x = x+1
 
 if __name__ == '__main__':
