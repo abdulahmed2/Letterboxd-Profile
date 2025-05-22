e2db1463b2455685fbd40cc9fa45f8632712f8ab
Abdul Ahmed <abdulrafay123ww@gmail.com>
2025-05-21 21:45:42 -0400

    dictionary made. link and more info tbd.

--- scraper-python.py
+++ scraper-python.py
@@ -12,6 +12,15 @@ def scrape():
     num_of_pages = soup.find_all('li', class_="paginate-page")
     x = 1
     for i in num_of_pages:
+        dictMovie = {
+        "Name": "FilmName",
+        "Year": 0,
+        "Rating": 0,
+        "Director": "DirectorName",
+        "Genre": "Genre",
+        "Theme1": "Theme1",
+        "Theme2": "Theme2"
+        }
         url = f'https://letterboxd.com/{username}/films/by/entry-rating/page/{x}/'
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -20,13 +29,18 @@ def scrape():
             img = film.find('img')
             if img and img.has_attr('alt'):
                 print(img['alt'])
+            dictMovie.update({"Name": img['alt']})
             rating = film.find('span', class_='rating')
             listOfClass = rating['class']
             rateNum = str(listOfClass[-1:])
             if len(str(rateNum)) == 12:
                 print('Rated ' + str(rateNum[-4:-2]) + '/10')
+                dictMovie.update({"Rating": int(rateNum[-4:-2])})
             elif len(str(rateNum)) == 11:
                 print('Rated ' + str(rateNum[-3:-2]) + '/10')
+                dictMovie.update({"Rating": int(rateNum[-3:-2])})
+            filmLink = film.find('a', class_='frame')
+            print(filmLink)
         x = x+1
 
 if __name__ == '__main__':
