cad68609b9b8e42692308f6d886dee80ffa0052c
Abdul-Rafay Ahmed <abdulrafay123ww@gmail.com>
2025-11-12 20:13:40 -0500

    just some chill stuff ngl #weGood

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ﻿# BeautifulSoup4 webscraper built to analyze and display user's top stats on Film site Letterboxd
-# Uses Flask to link Python backend script with HTML frontend built with TailwindCSS and a virtual environment
+Uses Flask to link Python backend script with HTML frontend built with TailwindCSS and a virtual environment
+
--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def scrape():
                     
                     filmLinkDiv = film.find('div', class_='poster')
                     url = 'https://letterboxd.com' + str(filmLinkDiv.get('data-target-link'))
-                    print(url)
+                    
                     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101     Firefox/108.0"}
                     response = requests.get(url, headers=headers)
                     soup = BeautifulSoup(response.text, 'html.parser')
